b58c72b51c21a1ecba6f4e0ac40bdf022974b534
juantaborda27 <juantabordaacostamail.com>
2025-07-16 00:27:02 -0500

    Servicios Comunidad y Parroquia funcionando

---
+++ src/supabase/storeServicios.jsx
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { supabase } from "./supabase.config";
+
+
+export const useStoreServicios = create((set) => ({
+    serviciosComunidad: [],
+    serviciosParroquia: [],
+    error: null,
+    loading: false,
+    currentServicio: null,
+
+    fetchServiciosComunidad: async () => {
+        set({ loading: true, error: null });
+        try {
+            const { data, error } = await supabase.from("Servicios").select("*").eq("tipo_servicio", 'Comunidad');;
+            if (error) throw error;
+            console.log("Servicios cargados comunidad:", data);
+            set({ serviciosComunidad: data, loading: false });
+        } catch (error) {
+            set({ error: error.message, loading: false });
+            alert("Error al cargar los servicios: " + error.message);
+        }
+    },
+
+    fetchServicioParroquia: async () => {
+        set({ loading: true, error: null });
+        try {
+            const { data, error } = await supabase.from("Servicios").select("*").eq("tipo_servicio", 'Parroquia');
+            if (error) throw error;
+            console.log("Servicios de parroquia cargados:", data);
+            set({ serviciosParroquia: data, loading: false });
+        } catch (error) {
+            set({ error: error.message, loading: false });
+            alert("Error al cargar los servicios de parroquia: " + error.message);
+        }
+    }
+}))
