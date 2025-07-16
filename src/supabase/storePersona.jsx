56412e481a96f257cd9c8d90c9bbb58f9c85db2d
juantaborda27 <juantabordaacostamail.com>
2025-07-16 00:38:07 -0500

    Archivo de store persona creado

---
+++ src/supabase/storePersona.jsx
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+import { supabase } from "./supabase.config";
+
+
+export const useStorePersona = create((set) => ({
+    persona: null,
+    error: null,
+    loading: false,
+    currentPersona: null,
+
+    addPersona: async (personaData, infoComunidad, Parroquia, sercComunidad, sercParroquia) => {
+        
+    }
+}))
