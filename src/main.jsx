b8039aba10fc56f994b949dea3feb274a9c917c6
Diego Yelamo <dyelamo@unicesar.edu.co>
2025-07-21 10:52:34 -0500

    HImplementación de una página de inicio de sesión con fondo de carrusel y funcionalidad de formulario.
    - Se añadió el componente de inicio de sesión con gestión de estado para las credenciales de usuario.
    - Se implementó un carrusel de imágenes de fondo que cambia cada 5 segundos.
    - Se creó un formulario de inicio de sesión adaptable con campos de entrada de usuario y contraseña.
    - Se diseñó la página de inicio de sesión con CSS para una apariencia moderna.
    - Se incluyó un pie de página con información de derechos de autor y un mensaje para la comunidad.

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FormularioRegistro  from "./pages/FormularioRegistro.jsx";
 import { Dashboard } from "./pages/Dashboard.jsx";
+import { Login } from "./pages/Login.jsx";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
         <Routes>
           <Route path="/FormularioRegistro" element={<FormularioRegistro/>}  />
-          <Route path="/" element={<Dashboard/>}  />
+          <Route path="/Dashboard" element={<Dashboard/>}  />
+          <Route path="/" element={<Login/>}/>
         </Routes>  
   </BrowserRouter>
 )
