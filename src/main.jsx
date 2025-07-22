fec149fafbf66719f0c296e70d33d9f52e52ee46
juantaborda27 <juantabordaacostamail.com>
2025-07-21 23:38:07 -0500

    Ruta protegida

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FormularioRegistro  from "./pages/FormularioRegistro.jsx";
 import { Dashboard } from "./pages/Dashboard.jsx";
 import { Login } from "./pages/Login.jsx";
+import PrivateRoute from './utils/PrivateRoutes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
         <Routes>
           <Route path="/FormularioRegistro" element={<FormularioRegistro/>}  />
-          <Route path="/Dashboard" element={<Dashboard/>}  />
           <Route path="/" element={<Login/>}/>
+
+
+          <Route 
+            path="/Dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+
         </Routes>  
   </BrowserRouter>
 )
